dd2890a98f1d346886201f3bd348ea75a1665f4c
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-04-06 18:42:20 -0300

    entrega requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((planeta) => (
+          <PlanetCard
+            key={ planeta.name }
+            planetName={ planeta.name }
+            planetImage={ planeta.image }
+          />))}
       </div>
     );
   }
